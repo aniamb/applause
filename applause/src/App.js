4cbf5556704ef15578e67e431fa18e1358024eca
Jagat <Jagat@purdue.edu>
2020-09-18 16:42:11 -0400

    Added Mongoose and Mongo
    - Initiated The Back-end Project
    - Installed MondoDB
    - Created A New MongoDB Database (use applause)
    -Connected To MongoDB By Using Mongoose

--- applause/src/App.js
+++ applause/src/App.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-fucntion App () {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // <Router>
+        /* <Route path="/" exact component={TodosList} /> */
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      // </Router>
+    );
+  }
 }
 
 export default App;
--- applause/src/backend/server.js
+++ applause/src/backend/server.js
@@ -1,8 +1,32 @@
 
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // console.log that your server is up and running
+// app.listen(port, () => console.log(`Listening on port ${port}`))
+
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 4000;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/applause', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
 
