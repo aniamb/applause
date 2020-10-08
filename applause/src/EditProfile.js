af31c440cdc360a8ac675f0378c5ec0c6b19e133
Puja Maheshwari <pujam1231@gmail.com>
2020-10-08 09:23:49 -0400

    save changes implemented

--- applause/src/EditProfile.js
+++ applause/src/EditProfile.js
@@ -24,24 +24,35 @@ class EditProfile extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-        user:user,
+        firstname:"",
+        lastname:"",
+        handle:"",
+        bio:"",
+        // user:user,
         edit:false
     }
+    this.handleHandleChange = this.handleHandleChange.bind(this);
+    this.handleFirstnameChange = this.handleFirstnameChange.bind(this);
+    this.handleLastnameChange = this.handleLastnameChange.bind(this);
+    this.handleBioChange = this.handleBioChange.bind(this);
 }
 
 componentDidMount(){
     console.log("component mounted");
 
+    var localEmail = localStorage.getItem('currentUser');
     //need to change this to use local storage
-    var lookupUser = user.email;
+    // var lookupUser = user.email;
+    localEmail="pujam123@gmail.com";
     axios.get('http://localhost:5000/profile', {
         params: {
-            email:lookupUser
+            email:localEmail
         }
     })
     .then((response) => {        
         console.log("response received.");
-        this.setState({user: response.data});
+        this.setState({handle: response.data.handle, firstname: response.data.firstname, 
+            lastname: response.data.lastname, bio: response.data.bio});
     })
     .catch((err) => {
         console.log('error getting info');
@@ -52,29 +63,60 @@ editProfile = () => {
     this.setState({edit:true});
 }
 
+handleHandleChange(event) {
+    this.setState({handle: event.target.value});
+}
+
+handleFirstnameChange(event) {
+    this.setState({firstname: event.target.value});
+}
+
+handleLastnameChange(event) {
+    this.setState({lastname: event.target.value});
+}
+
+handleBioChange(event) {
+    this.setState({bio: event.target.value});
+}
+
+handleSubmit(event){
+    event.preventDefault();
+    event.target.reset();
+
+    var currUserEmail = localStorage.getItem('currentUser');
+    currUserEmail = "pujam123@gmail.com";
+    const updateInfo = {handle:this.state.handle, firstname: this.state.firstname, lastname: this.state.lastname, bio:this.state.bio, currUserEmail:currUserEmail}
+    axios.post('http://localhost:5000/editprofile', updateInfo). then(response => {
+        console.log("Edited profile successfully.");
+    }).catch((err) => {
+        console.log("Edit profile failed.");
+    })
+}
+
 render() {
   return (
     <div className="CreateAccount">
         <div className="container">
             <div className="left">
                 <FontAwesomeIcon className="prof" icon={faUserCircle} size="sm"/>
-                <form>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <label>Handle</label>
-                    <input type="text" id="username" value={this.state.user.handle}/> 
+                    <input type="text" id="username" value={this.state.handle} onChange={this.handleHandleChange.bind(this)}/> 
                     <br></br>
 
                     <label>First Name</label>
-                    <input type="text" id="name" value={this.state.user.firstname}/> 
+                    <input type="text" id="name" value={this.state.firstname} onChange={this.handleFirstnameChange.bind(this)}/> 
                     <br></br>
 
                     <label>Last Name</label>
-                    <input type="text" id="name" value={this.state.user.lastname}/> 
+                    <input type="text" id="name" value={this.state.lastname} onChange={this.handleLastnameChange.bind(this)}/> 
                     <br></br>
 
                     <label>Bio</label>
-                    <textarea rows="3" cols="20" name="bio" value={this.state.user.bio} />
+                    <textarea rows="3" cols="20" name="bio" value={this.state.bio} onChange={this.handleBioChange.bind(this)}/>
+                    
+                    <input className="submit" type="submit" value="Save Changes"/>
                 </form>
-                <input className="submit" type="submit" value="Save Changes"/>
             </div>
         </div>
     </div>
