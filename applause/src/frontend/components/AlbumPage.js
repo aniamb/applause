908e5ceecaec15c50f23698ab399c4a125e9a3e0
Anita <aambalav@purdue.edu>
2020-10-20 16:09:54 -0400

    basic frontend

--- applause/src/frontend/components/AlbumPage.js
+++ applause/src/frontend/components/AlbumPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, Redirect} from 'react-router-dom'
 import '../styles/Profile.css';
 import axios from 'axios'
+import exampleImg from './gkmc.jpg';
 
 
 
@@ -27,11 +28,14 @@ render() {
     <div className="CreateAccount">
         <div className="container">
             <div className="left">
-                <h1>yea</h1>
-
+                <h1>Album Name</h1>
+                <h2>Artist Name</h2>
+                <img src={exampleImg}></img>
+                <br></br>
+                <input type="submit" value="Review this Album" />
 
             </div>
-
+            <div className="right">This is where the user's own reviews would be!</div>
         </div>
     </div>
 
