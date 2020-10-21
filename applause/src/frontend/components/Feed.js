4edb66b9faeb0a0c9a8715edca7690366f7c9ff6
Anita <aambalav@purdue.edu>
2020-10-21 17:11:51 -0400

    idk

--- applause/src/frontend/components/Feed.js
+++ applause/src/frontend/components/Feed.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+ import React, {Component, Fragment} from 'react';
 import { NavLink, Redirect} from 'react-router-dom'
 import axios from 'axios'
 import '../../App.css';
