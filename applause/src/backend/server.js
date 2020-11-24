670bdc4e908f4983d203260d700d8cc299613daf
Anita <aambalav@purdue.edu>
2020-11-24 17:18:55 -0500

    redirect back to profile page

--- applause/src/backend/server.js
+++ applause/src/backend/server.js
@@ -136,7 +136,7 @@ app.post('/searchserver', function (req,res1) {
 
 var client_id = '1e16a710b11b440080bd4f588115191e'; // Your client id
 var client_secret = '10443ec704464dc990af339e1bbee0f3'; // Your secret
-var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:5000/callback'; // Your redirect uri
 
 var generateRandomString = function(length) {
    var text = '';
@@ -169,6 +169,73 @@ app.get('/spotifyauth', function(req, res) {
    }));
       
       console.log("basic redirect");
+ });
+
+ app.get('/callback', function(req, res) {
+
+   console.log("idk if htis works");
+
+   // your application requests refresh and access tokens
+   // after checking the state parameter
+   
+   var code = req.query.code || null;
+   var state = req.query.state || null;
+   var storedState = req.cookies ? req.cookies[stateKey] : null;
+ 
+   if (state === null || state !== storedState) {
+     res.redirect('/#' +
+       querystring.stringify({
+         error: 'state_mismatch'
+       }));
+   } else {
+     res.clearCookie(stateKey);
+     var authOptions = {
+       url: 'https://accounts.spotify.com/api/token',
+       form: {
+         code: code,
+         redirect_uri: redirect_uri,
+         grant_type: 'authorization_code'
+       },
+       headers: {
+         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+       },
+       json: true
+     };
+ 
+     request.post(authOptions, function(error, response, body) {
+       if (!error && response.statusCode === 200) {
+ 
+         var access_token = body.access_token,
+             refresh_token = body.refresh_token;
+ 
+         var options = {
+           url: 'https://api.spotify.com/v1/me',
+           headers: { 'Authorization': 'Bearer ' + access_token },
+           json: true
+         };
+ 
+         // use the access token to access the Spotify Web API
+         request.get(options, function(error, response, body) {
+         console.log("hi");
+           console.log(body);
+         });
+ 
+         // we can also pass the token to the browser to make requests from there
+         // res.redirect('/#' +
+         //   querystring.stringify({
+         //     access_token: access_token,
+         //     refresh_token: refresh_token
+         //   }));
+
+         res.redirect('http://localhost:3000/profile');
+       } else {
+         res.redirect('/#' +
+           querystring.stringify({
+             error: 'invalid_token'
+           }));
+       }
+     });
+   }
  });
 
 //createreview
